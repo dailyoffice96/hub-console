07370bc7b8b08f8bb783f73e870b044016d63b1e
본인dailyoffice96-home <dailyoffice96@gmail.com>
2026-07-28 00:06:45 +0900

    Admin  Completed and User Working 

--- backend/src/main/java/com/smconsole/user/User.java
+++ backend/src/main/java/com/smconsole/user/User.java
@@ -1,6 +1,45 @@
 ﻿package com.smconsole.user;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long id;
+
+    @Column(name="user_name", nullable = false)
+    private String name;
+
+    @Column(name = "login_id", nullable = false, unique = true)
+    private String loginId;
+
+    private String phone;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column(name="created_at")
+    private LocalDate createdAt = LocalDate.now();
+
+    @Column(name="with_drawn_at")
+    private LocalDate withdrawnAt;
+
+    @Column(name="up_date_at")
+    private LocalDate updatedAt;
 
 }
 

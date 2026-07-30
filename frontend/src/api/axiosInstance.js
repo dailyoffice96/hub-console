623aa8d53fc4a6f5e3348296c4155e03384872c4
dailyoffice96-academy <dailyoffice96@gmail.com>
2026-07-30 18:19:36 +0900

    login --> ing header --> end sidebar --> end

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,11 @@
 ﻿import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://15.164.80.9:9000',
-  withCredentials: true,
+  baseURL: 'http://localhost:9000',
+  withCredentials: true, // "쿠키도 같이 보내라"
+/*  1. 로그인 성공 → 서버가 세션 쿠키를 발급해서 브라우저에게 줌
+  2. 이후 요청마다, 브라우저가 그 쿠키를 자동으로 담아서 보내야 함
+  3. 서버가 그 쿠키를 보고 "아, 이 사람 로그인된 사람이구나" 확인*/
 });
 
 export default axiosInstance;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import {useLocation, useNavigate} from "react-router-dom"
+import { logout } from '../api/authApi';
+import {useState} from "react"
+
+function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [myInfo, setMyInfo] = useState("");
+
+    const pageTitles = {
+        '/users': '회원관리',
+        '/admins': '관리자관리',
+        '/inquiries': '문의관리',
+        '/incidents': '장애관리',
+        '/audit-logs': '감사로그',
+    };
+
+    const title = pageTitles[location.pathname] || 'SM Console';
+
+    const handleLogout = async() => {
+     await logout();
+     navigate('/login');
+    }
+
+    //화면이 맨 처음 만들어질 때 딱 한 번, 서버의 /api/me라는 주소로 조회 요청을 보내고,
+    //응답이 오면 그 데이터를 myInfo에 저장해라
+    useEffect(() => {
+        axiosInstance.get('/api/me').then(res => setMyInfo(res.data));
+    }, []);
+
+    return(
+    <div className="p-3 d-flex justify-content-between align-items-center">
+       <h4>{title}</h4>
+
+       <div className="d-flex align-items-center">
+       <span className="me-3">{myInfo?.name} · {myInfo?.role}</span>
+       <button className="btn btn-outline-secondary btn-sm" onClick={handleLogout}>로그아웃</button>
+       </div>
+
+    </div>
+    )
+}
+
+export default Header;

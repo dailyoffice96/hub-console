99825c14a03f3e1af8f2a6a5e39e71a37d652235
dailyoffice96 <dailyoffice96@gmail.com>
2026-08-06 12:33:08 +0900

    fix: 배포 환경에 따른 CORS 허용 범위 수정

--- backend/src/main/java/com/smconsole/common/exception/ErrorResponse.java
+++ backend/src/main/java/com/smconsole/common/exception/ErrorResponse.java
@@ -2,5 +2,18 @@ package com.smconsole.common.exception;
 
 public class ErrorResponse {
 
+    private String message;
+
+    public ErrorResponse(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
 
--- backend/src/main/java/com/smconsole/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/smconsole/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,23 @@
 package com.smconsole.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice //어디서든 예외가 터지면, 얘한테 먼저 물어봐줘.(전역 감시자 역할)
 public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("서버 오류가 발생했습니다."));
+    }
 
 }
 

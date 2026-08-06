2ad236f7be09d4ad402f15cc3059e5beb5b6acf3
dailyoffice96-academy <dailyoffice96@gmail.com>
2026-08-06 12:43:33 +0900

    Vercel 재배포

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 ﻿import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://sm-console-production-6596.up.railway.app',
+  baseURL: '',
   withCredentials: true,
     headers: {
       'Cache-Control': 'no-cache',
